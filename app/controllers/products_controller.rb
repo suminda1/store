9b46bc4d5864cfd945a59cd7b8cb4adae374bde5
suminda <suminda@gmail.com>
2016-02-14 15:03:22 +1030

    Update Https certificate issue
    update https certificate in Gemfile and update bundle

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,12 +25,26 @@ class ProductsController < ApplicationController
    
    def import
        Product.delete_all
-       file_data = params[:file].read
-       csv_rows  = CSV.parse(file_data)
+       #t = RemoteTable.new("https://dl.dropboxusercontent.com/u/6582068/products.csv")
+       #t.each do |row|
+       # puts row['foo']
+       #end
+      #url = URI.parse("https://dl.dropboxusercontent.com/u/6582068/products.csv")
+      CSV.new(open("https://dl.dropboxusercontent.com/u/6582068/products.csv"), :headers => :first_row).each do |row|
+         Product.create! productid: row[0], category: row[1], name: row[2], price: row[3]
+      end
+      
+       
+       #file_data =  params[:file].read
+       #https://dl.dropboxusercontent.com/u/6582068/products.csv
+       #url = URI.parse("https://dl.dropboxusercontent.com/u/6582068/products.csv")
 
-       csv_rows.each do |row|
-       Product.create! productid: row[0], category: row[1], name: row[2], price: row[3]
-       end
+       #file_data = open(url).read()
+       #csv_rows  = CSV.parse(file_data)
+
+       #csv_rows.each do |row|
+       #Product.create! productid: row[0], category: row[1], name: row[2], price: row[3]
+       #end
        redirect_to products_path
   
    end
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rubygems'
+require 'open-uri'
 require 'csv'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
